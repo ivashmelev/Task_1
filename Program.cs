01a5542dafe0b508cc6db9491cd6de7d7a93578c
Шмелев Иван <ishmelev@1ditis.ru>
2018-06-13 15:48:59 +0300

    ready

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите диаметр окружности");
+            int diametr = int.Parse(Console.ReadLine());
+            const double P=3.14;
+            var length = diametr * P;
+            var radius = diametr / 2;
+            var square = P * (radius * radius);
+            Console.WriteLine($"Длина окружности: {length}");
+            Console.WriteLine($"Площадь круга: {square}");
+            Console.ReadKey();
+        }
+    }
+}
